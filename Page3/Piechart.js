91f3df2906181b92a72729bee4a2e3595dcbe5da
Sira <kongsirisira@gmail.com>
2023-08-16 16:44:46 +0700

    finallllll

--- Page3/Piechart.js
+++ Page3/Piechart.js
@@ -1,40 +1,46 @@
 var ctx = document.getElementById('myChart').getContext('2d');
 
-  // Define the data for the pie chart
-  var data = {
-    datasets: [{
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'red',
-        'blue',
-        'yellow',
-        'green',
-        'purple',
-        'orange'
-      ]
-    }]
-  };
-
-  const config = 
-{
-    type: 'pie',
-    data: data , 
-    options: 
-    {
-        plugins:
-        {
-            legend:
-            {
-                display: false
-            },
-        },
-        responsive: true,
-        maintainAspectRatio: false
-    }
-
+// Define the data for the pie chart
+var data = {
+  labels: ['fashion & grooming', 'health & sports', 'art & design', 'lifestyle', 'art & design', 'entertainment', 'technology & car', 'art & design '],
+  datasets: [{
+    data: [12140, 5833, 5611, 4563, 3214, 2024, 1781, 1612],
+    backgroundColor: [
+      'red',
+      'blue',
+      'yellow',
+      'green',
+      'purple',
+      'orange'
+    ]
+  }]
 };
-  // Create the pie chart
-  var myChart = new Chart(
-    document.getElementById('myChart'),
-    config
-  );
+
+const config = {
+  type: 'pie',
+  data: data,
+  options: {
+    plugins: {
+      legend: {
+        display: false
+      },
+      tooltip: {
+        callbacks: {
+          label: function (context) {
+            var label = data.labels[context.dataIndex];
+            var value = context.dataset.data[context.dataIndex];
+            return label + ': ' + value;
+          }
+        }
+      }
+    },
+    responsive: true,
+    maintainAspectRatio: false
+  }
+};
+
+// Create the pie chart
+var myChart = new Chart(
+  document.getElementById('myChart'),
+  config
+);
