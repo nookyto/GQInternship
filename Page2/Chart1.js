6422d5f52ddcdcf2dfd5a98b50f3c215c487fc1a
Sira <kongsirisira@gmail.com>
2023-08-28 16:03:08 +0700

    psge3

--- Page2/Chart1.js
+++ Page2/Chart1.js
@@ -7,7 +7,7 @@ let DailyVoteChart; // Declare the DailyVoteChart variable
 DailyVoteChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ['1 Jun', '2 Jun', '3 Jun', '4 Jun', '5 Jun', '6 Jun', '7 Jun', '8 Jun', '9 Jun', '10 Jun', '11 Jun', '12 Jun', '13 Jun', '14 Jun', '15 Jun', '16 Jun', '17 Jun', '18 Jun', '19 Jun', '20 Jun', '21 Jun', '22 Jun', '23 Jun', '24 Jun', '25 Jun', '26 Jun', '27 Jun', '28 Jun', '29 Jun', '30 Jun', '1 July', '2 July', '3 July', '4 July', '5 July', '6 July', '7 July', '8 July', '9S July'],
+    labels: ['1 Jun', '2 Jun', '3 Jun', '4 Jun', '5 Jun', '6 Jun', '7 Jun', '8 Jun', '9 Jun', '10 Jun', '11 Jun', '12 Jun', '13 Jun', '14 Jun', '15 Jun', '16 Jun', '17 Jun', '18 Jun', '19 Jun', '20 Jun', '21 Jun', '22 Jun', '23 Jun', '24 Jun', '25 Jun', '26 Jun', '27 Jun', '28 Jun', '29 Jun', '30 Jun', '1 July', '2 July', '3 July', '4 July', '5 July', '6 July', '7 July', '8 July', '9 July'],
     datasets: [{
       label: '# of Votes',
       data: [104,55,2,11,1,22,75,35,6,2,33,4,16,27,9,20,14,24,76,580,1670,2708,3875,5289,1076,1352,1476,1324,1345,1252,1001,695,937,1560,1291,1429,1381,1300,967],
